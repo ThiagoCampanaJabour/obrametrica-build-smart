86eb51e3c50f25d931b384b3c90a19320a18fdf7
ThiagoCampanaJabour <thiagocampanajabour@gmail.com>
2026-07-09 16:47:11 -0300

    fix: update eslint react-refresh config with allowExportNames

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,21 @@ export default tseslint.config(
           ],
         },
       ],
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowExportNames: [
+            "loader",
+            "action",
+            "meta",
+            "links",
+            "handle",
+            "ErrorBoundary",
+            "clientLoader",
+            "clientAction",
+          ],
+        },
+      ],
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
