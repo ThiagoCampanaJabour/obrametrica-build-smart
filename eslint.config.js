db48dee4ef01976e960527113ebf453ef5356af3
ThiagoCampanaJabour <thiagocampanajabour@gmail.com>
2026-07-09 16:49:21 -0300

    fix: disable react-refresh warning

--- eslint.config.js
+++ eslint.config.js
@@ -32,21 +32,7 @@ export default tseslint.config(
           ],
         },
       ],
-      "react-refresh/only-export-components": [
-        "warn",
-        {
-          allowExportNames: [
-            "loader",
-            "action",
-            "meta",
-            "links",
-            "handle",
-            "ErrorBoundary",
-            "clientLoader",
-            "clientAction",
-          ],
-        },
-      ],
+      "react-refresh/only-export-components": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
